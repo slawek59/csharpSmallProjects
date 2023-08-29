4f9df2e64020a5f3334ff82b98d2112b2a831f6b
Slawomir Was <was.slawek@gmail.com>
2023-08-29 12:52:10 +0200

    ATM

--- Program.cs
+++ Program.cs
@@ -80,11 +80,90 @@ public class cardHolder
 
         void deposit(cardHolder currentUser)
         {
-            Console.WriteLine("How much money would you like to deposit? ");
+            Console.WriteLine("How much money would you like to deposit: ");
             double deposit = Double.Parse(Console.ReadLine());
-            currentUser.setBalance(deposit);
-            Console.WriteLine("Your balance is: " + currentUser.getBalance());
+            currentUser.setBalance(currentUser.getBalance() + deposit);
+            Console.WriteLine("Your new balance is: " + currentUser.getBalance());
         }
+
+        void withdraw(cardHolder currentUser)
+        {
+            Console.WriteLine("How much money would you like to withdraw: ");
+            double withdrawal = Double.Parse(Console.ReadLine());
+            //check if the user has enough money
+            if(currentUser.getBalance() < withdrawal)
+            {
+                Console.WriteLine("Insufficient balance :(");
+            }
+            else
+            {
+                currentUser.setBalance(currentUser.getBalance() - withdrawal);
+                Console.WriteLine("You're good to go, thank you."); 
+            }    
+        }
+
+        void balance(cardHolder currentUser)
+        {
+            Console.WriteLine("Current balance: " + currentUser.getBalance());
+        }
+
+        List<cardHolder> cardHolders = new List<cardHolder>();
+        cardHolders.Add(new cardHolder("1111111111111111", 1234, "Luke", "Skywalker", 10000.00));
+        cardHolders.Add(new cardHolder("2222222222222222", 2345, "Han", "Solo", 10000.00));
+        cardHolders.Add(new cardHolder("3333333333333333", 3456, "Leia", "Organa", 100000.00));
+        cardHolders.Add(new cardHolder("4444444444444444", 4567, "Darth", "Vader", 1000000000.15));
+        cardHolders.Add(new cardHolder("5555555555555555", 5678, "Bohhuah", "Mutdah", 1000000000000000.00));
+        
+        //Prompt user
+        Console.WriteLine("Welcome to SimpleATM");
+        Console.WriteLine("Please insert your debit card: ");
+        String debitCardNum = "";
+        cardHolder currentUser;
+
+        while(true)
+        {
+            try
+            {
+                debitCardNum = Console.ReadLine();
+                currentUser = cardHolders.FirstOrDefault(a => a.cardNum == debitCardNum);
+                if (currentUser != null) { break; }
+                else { Console.WriteLine("Card not recognized. Please try again"); }
+            }
+            catch { Console.WriteLine("Card not recognized. Please try again"); }
+        }
+
+        Console.WriteLine("Please enter your pin: ");
+        int userPin = 0;
+        while (true)
+        {
+            try
+            {
+                userPin = int.Parse(Console.ReadLine());
+                if (currentUser.getPin() == userPin) { break; }
+                else { Console.WriteLine("Incorrect pin. Please try again"); }
+            }
+            catch { Console.WriteLine("Incorrect pin. Please try again"); }
+        }
+
+        Console.WriteLine("Welcome " + currentUser.getFirstName() + " :)");
+        int option = 0;
+        do
+        {
+            printOptions();
+            try
+            {
+                option = int.Parse(Console.ReadLine());
+            }
+            catch{}
+            if (option == 1) { deposit(currentUser); }
+            else if (option == 2) { withdraw(currentUser); }    
+            else if (option == 3) { balance(currentUser); } 
+            else if (option == 4) { break; }
+            else { option = 0; }
+
+        } while (option != 4);
+        Console.WriteLine("Thank you! Have a nice day.");    
     }
 
-}
+
+} 
