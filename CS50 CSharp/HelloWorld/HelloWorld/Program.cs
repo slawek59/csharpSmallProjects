c2fba17abeb5afe2365b69708cf252da7709d794
Slawomir Was <was.slawek@gmail.com>
2023-09-05 21:52:09 +0200

    HelloWorld

---
+++ CS50 CSharp/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+
+            Console.WriteLine("hello, " + GetString());
+        }
+
+        public static string GetString()
+        {
+            Console.Write("What's your name? ");
+            var name = Console.ReadLine();
+
+            return name;
+        }
+    }
+}
