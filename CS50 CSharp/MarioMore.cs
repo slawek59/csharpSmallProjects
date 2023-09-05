4bc5fbc2b0edec420d358807667163f4a6b46216
Slawomir Was <was.slawek@gmail.com>
2023-09-05 23:28:29 +0200

    MarioMore.cs

---
+++ CS50 CSharp/MarioMore.cs
@@ -0,0 +1,56 @@
+﻿namespace MarioMore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int heigth = GetHeight();
+
+            PrintPyramid(heigth);
+
+        }
+
+        public static int GetHeight()
+        {
+            var input = "";
+
+            do
+            {
+                Console.Write("Height: ");
+                input = Console.ReadLine();
+            }
+            while (String.IsNullOrWhiteSpace(input) || Convert.ToInt32(input) < 1 || Convert.ToInt32(input) > 8);
+
+            var height = Convert.ToInt32(input);
+
+            return height;
+        }
+
+        public static void PrintPyramid(int height)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int k = height - i; k > 0; k--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0 + i; j > 0; j--)
+                {
+                    Console.Write("#");
+                }
+
+                Console.Write("# #");
+
+                for (int l = height - i; l < height; l++)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine(" ");
+
+            }
+        }
+    }
+}
